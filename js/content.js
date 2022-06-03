44bebf001eb1c391f3f0d69187effc1a055440fe
ldH <ldh19971027@gmail.com>
2022-06-03 16:54:20 +0900

    반복되는 부분 주석 추가

--- js/content.js
+++ js/content.js
@@ -29,19 +29,21 @@ const paintYearMonth = () => {
 
 //* 이번달 date 그리는 함수
 const paintThisDate = () => {
-
+   /* 반복 하는 부분 */
     const lastDate = new Date(year, monthIndex + 1, 0).getDate();
     const firstDay = new Date(year, monthIndex, 0).getDay() + 1;
 
     let thisMonthDate = 1;
     let lastMonthDate = new Date(year, monthIndex, 0).getDate() - (firstDay - 1);
 
-    for (let i = 0; i < firstDay; i++) {
-        const dateTag = document.createElement("div");
-        dateTag.innerText = lastMonthDate;
-        dateTag.className = "last-month-date";
-        date.appendChild(dateTag);
-        lastMonthDate++;
+    if (firstDay !== 7) {
+        for (let i = 0; i < firstDay; i++) {
+            const dateTag = document.createElement("div");
+            dateTag.innerText = lastMonthDate;
+            dateTag.className = "last-month-date";
+            date.appendChild(dateTag);
+            lastMonthDate++;
+        }
     }
     for (let i = 0; i <= lastDate; i++) {
         const dateTag = document.createElement("div");
@@ -52,19 +54,23 @@ const paintThisDate = () => {
         date.appendChild(dateTag);
         thisMonthDate++;
     }
+    /* 반복 하는 부분 */
 }
 
 //* 저번 달 넘어가는 함수
-const handlePrevMonth = () => {
+const handlenextMonth = () => {
     date.innerHTML = "";
     if (monthIndex === 0) {
-        monthIndex = 12;
+        monthIndex = 11;
         year -= 1;
         console.log(year);
+    } else {
+        monthIndex -= 1;
     }
-    monthIndex = monthIndex - 1;
-    const prevMonth = monthList[monthIndex];
-    month.innerText = `${prevMonth}${year}`
+
+    /* 반복되는 부분 */
+    const nextMonth = monthList[monthIndex];
+    month.innerText = `${nextMonth}${year}`
 
     const lastDate = new Date(year, monthIndex + 1, 0).getDate();
     const firstDay = new Date(year, monthIndex, 0).getDay() + 1;
@@ -89,6 +95,7 @@ const handlePrevMonth = () => {
         date.appendChild(dateTag);
         thisMonthDate++;
     }
+    /* 반복되는 부분 */
 }
 
 //* 다음 달 넘어가는 함수
@@ -97,10 +104,40 @@ const handleNextMonth = () => {
     if (monthIndex === 11) {
         monthIndex = 0;
         year += 1;
+    } else {
+        monthIndex += 1;
     }
+
+    const nextMonth = monthList[monthIndex];
+    month.innerText = `${nextMonth}${year}`
+
+    const lastDate = new Date(year, monthIndex + 1, 0).getDate();
+    const firstDay = new Date(year, monthIndex, 0).getDay() + 1;
+
+    let thisMonthDate = 1;
+    let lastMonthDate = new Date(year, monthIndex, 0).getDate() - (firstDay - 1);
+    if (firstDay !== 7) {
+        for (let i = 0; i < firstDay; i++) {
+            const dateTag = document.createElement("div");
+            dateTag.innerText = lastMonthDate;
+            dateTag.className = "last-month-date";
+            date.appendChild(dateTag);
+            lastMonthDate++;
+        }
+    }
+    for (let i = 1; i <= lastDate; i++) {
+        const dateTag = document.createElement("div");
+        dateTag.innerText = thisMonthDate;
+        if (time.getDate() == thisMonthDate) {
+            dateTag.className = "today";
+        }
+        date.appendChild(dateTag);
+        thisMonthDate++;
+    }
+
 }
 
 window.addEventListener("load", paintYearMonth);
 window.addEventListener("load", paintThisDate);
-prevBtn.addEventListener("click", handlePrevMonth);
+prevBtn.addEventListener("click", handlenextMonth);
 nextBtn.addEventListener("click", handleNextMonth);
