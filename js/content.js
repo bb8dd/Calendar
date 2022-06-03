f202e50d5ffb7140f6db7cae305ba0e5203dd8e5
ldH <ldh19971027@gmail.com>
2022-06-03 09:20:29 +0900

    getFullYear, getMonth

--- js/content.js
+++ js/content.js
@@ -1,4 +1,10 @@
 const month = document.querySelector(".month");
 
-const date = new Date();
-console.log(date.getMonth);
+const getYearMonth = () => {
+    const date = new Date();
+    const dateMonth = String(date.getMonth() + 1).padStart(2, "0");
+    const dateYear = String(date.getFullYear());
+    month.innerText = `${dateYear}.${dateMonth}`
+}
+
+window.addEventListener("load", getYearMonth)
